6e3d374321f6afe78625882c03d0e319aaeb703a
korya <korya@github.com>
2015-06-04 16:31:28 -0400

    Add README and usage example

---
+++ example/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	sitemap "github.com/PlanitarInc/go-sitemap"
+)
+
+type ArrayInput struct {
+	Arr     []SimpleEntry
+	NextIdx int
+}
+
+func (a ArrayInput) HasNext() bool {
+	return a.NextIdx < len(a.Arr)
+}
+
+func (a *ArrayInput) Next() sitemap.UrlEntry {
+	idx := a.NextIdx
+	a.NextIdx++
+	return a.Arr[idx]
+}
+
+type SimpleEntry struct {
+	Url       string
+	Modified  time.Time
+	ImageUrls []string
+}
+
+func (e SimpleEntry) GetLoc() string {
+	return e.Url
+}
+
+func (e SimpleEntry) GetLastMod() time.Time {
+	return e.Modified
+}
+
+func (e SimpleEntry) GetImages() []string {
+	return e.ImageUrls
+}
+
+func main() {
+	entries := []SimpleEntry{
+		SimpleEntry{
+			Url:      "http://example.com/",
+			Modified: time.Now(),
+		},
+		SimpleEntry{
+			Url: "http://example.com/test/",
+			ImageUrls: []string{
+				"http://example.com/test/1.jpg",
+				"http://example.com/test/2.jpg",
+				"http://example.com/test/3.jpg",
+			},
+		},
+	}
+
+	sitemap.SitemapWrite(os.Stdout, &ArrayInput{Arr: entries})
+}
